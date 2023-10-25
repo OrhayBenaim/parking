7e03011b0f6fb9ae37a1c445a317341c546ccba1
Orhay Benaim <orhay.benaim@sunbit.com>
2023-10-25 10:45:48 +0300

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    tailwind(),
-    solidJs({
-      include: ["**/solid/*"],
-    }),
-  ],
+  integrations: [tailwind(), solidJs({
+    include: ["**/solid/*"]
+  })],
+  adapter: vercel()
 });
