18896a2ee1d6cdf8795b1f0aeb764e21ffaaf150
Orhay Benaim <orhay.benaim@sunbit.com>
2023-11-05 16:31:22 +0200

    added google tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), solidJs({
     include: ["**/solid/*"]
-  })],
+  }), partytown()],
   adapter: vercel()
 });
